1c6cc2830308c0a8578dd58eb04c22f0a9993a2b
techyjc <john@techbyte.co.uk>
2024-11-24 22:25:10 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+let charstring = "ABCDEFABCDEF";
+
+let charsarr = Array();
+let checkarr = Array();
+let founditem = 0;
+let finish = false;
+
+charsarr = [...charstring];
+
+charsarr.forEach((key) => {
+    founditem = checkarr.includes(key);
+    if (founditem) {
+        if (!finish) {
+            finish = true;
+            console.log(key);
+            console.log('Found');
+            return;
+        }
+    } else {
+        checkarr.push(key);
+    }
+});
+console.log(checkarr);
+
